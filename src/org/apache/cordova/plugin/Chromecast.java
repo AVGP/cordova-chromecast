1733e7eb4581d8163aa592432dc24a12e7b9cfaa
Martin N <avgp_2k@yahoo.de>
2014-02-19 08:50:18 +0100

    Dummy 'echo' implementation, Java.

---
+++ src/org/apache/cordova/plugin/Chromecast.java
@@ -0,0 +1,24 @@
+package org.apache.cordova.plugin;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Chromecast extends CordovaPlugin {
+    @Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if(action.equals("echo")) {
+			this.echo(args.getString(0), callbackContext);
+			return true;
+		}
+		return false;
+	}
+
+	private void echo(String msg, CallbackContext callback) {
+		callback.success(msg);
+	}
+}
+
